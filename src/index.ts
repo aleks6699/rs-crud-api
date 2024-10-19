aca50d7a292a2e8c7a4955acec49aa1e4e71c44a
Oleksandr Tsurkan <aleks6699@gmail.com>
2024-10-19 18:23:47 +0300

    feat: implements add method put

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ function getUserById(id: string, res: http.ServerResponse) {
     return res.end(JSON.stringify({ error: 'User not found' }));
   }
 }
-async function requestBodyParser(req: http.IncomingMessage): Promise<Omit<User, 'id'>> {
+async function requestBodyParser(req: http.IncomingMessage): Promise<User> {
   let body = '';
 
   return new Promise((resolve, reject) => {
@@ -80,9 +80,41 @@ async function createUser(req: http.IncomingMessage, res: http.ServerResponse) {
   }
 }
 
+async function getUserUpdateId(id: string, res: http.ServerResponse, req: http.IncomingMessage) {
+  const { username, age, hobbies } = await requestBodyParser(req);
+  if (!validate(id)) {
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 400;
+    return res.end(JSON.stringify({ error: 'Invalid userId format (not a UUID)' }));
+  }
+  if (!data.users.find((user) => user.id === id)) {
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 404;
+    return res.end(JSON.stringify({ error: 'User not found' }));
+  }
+  res.setHeader('Content-Type', 'application/json');
+  res.statusCode = 200;
+
+  data.users = data.users.map((user) => {
+    if (user.id === id) {
+      return { ...user, username, age, hobbies };
+    }
+    return user;
+  });
+
+  return res.end(JSON.stringify({ message: 'User updated successfully', user: { id, username, age, hobbies } }));
+
+
+}
+
+
+
+
 
 
 const server = http.createServer((req, res) => {
+  const id = req.url.split('/').pop();
+
   if (req.method === 'POST' && req.url === '/api/users') {
     return createUser(req, res);
   }
@@ -92,19 +124,24 @@ const server = http.createServer((req, res) => {
   }
 
   if (req.method === 'GET' && req.url?.startsWith('/api/users/')) {
-    const id = req.url.split('/').pop();
     return getUserById(id, res);
   }
 
   if (req.method === 'PUT' && req.url?.startsWith('/api/users/')) {
-    const id = req.url.split('/').pop();
-    // return getUserUpdateId(id, res);
+
+    return getUserUpdateId(id, res, req);
   }
+
+  // if (req.method === 'DELETE' && req.url?.startsWith('/api/users/')) {
+  //    return deleteUser(id, res);
+  // }
 
   res.statusCode = 404;
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify({ error: 'Not found' }));
 });
+
+
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);

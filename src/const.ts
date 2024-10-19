62423e7e1e0ae16267830f29d0c9adbcae0598f3
Oleksandr Tsurkan <aleks6699@gmail.com>
2024-10-19 03:16:16 +0300

    feat: implements get method

---
+++ src/const.ts
@@ -0,0 +1,25 @@
+import { Users } from './types/types';
+
+
+export const data: Users = {
+  users: [
+    {
+      id: '1e4b0a5c-2d6d-4bb6-a839-74576cbd2fbd',
+      username: 'John',
+      age: 30,
+      hobbies: ['reading', 'swimming']
+    },
+    {
+      id: '2f07a5c4-1c3f-4cb6-b739-64576bbd4fbc',
+      username: 'Jane',
+      age: 25,
+      hobbies: ['writing', 'reading']
+    },
+    {
+      id: '3e1c8a9f-2b4e-4876-89b2-64576ab13dcd',
+      username: 'Bob',
+      age: 40,
+      hobbies: ['swimming', 'cooking']
+    }
+  ]
+};
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,51 @@
-console.log('Hello World!');
+require('dotenv').config();
+import http from 'http';
+import { data } from './const';
+import { v4, validate } from 'uuid';
 
+const PORT = process.env.PORT || 3000;
+
+function getUsers(res: http.ServerResponse): void {
+  res.setHeader('Content-Type', 'application/json');
+  res.statusCode = 200;
+  res.end(JSON.stringify(data.users));
+}
+
+function getUserById(id: string, res: http.ServerResponse) {
+  if (!validate(id)) {
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 400;
+    return res.end(JSON.stringify({ error: 'Invalid userId format (not a UUID)' }));
+  }
+
+  const user = data.users.find((user) => user.id === id);
+
+  if (user) {
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+    return res.end(JSON.stringify(user));
+  } else {
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 404;
+    return res.end(JSON.stringify({ error: 'User not found' }));
+  }
+}
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/api/users') {
+    return getUsers(res);
+  }
+
+  if (req.method === 'GET' && req.url?.startsWith('/api/users/')) {
+    const id = req.url.split('/').pop();
+    return getUserById(id, res);
+  }
+
+  res.statusCode = 404;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ error: 'Not found' }));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
